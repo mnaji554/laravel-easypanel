f70b5aa7f80115f44265312673a1f8f9a2e9e577
mnaji554 <87244007+mnaji554@users.noreply.github.com>
2026-08-16 22:47:37 +0300

    Update child-sidebar-menu.blade.php

--- resources/views/layouts/child-sidebar-menu.blade.php
+++ resources/views/layouts/child-sidebar-menu.blade.php
@@ -1,5 +1,6 @@
 <li class="list-divider"></li>
 <li class="nav-small-cap"><span class="hide-menu">{{ __('CRUD Menu') }}</span></li>
 @foreach(\EasyPanel\Models\CRUD::active() as $crud)
-    <x-easypanel::crud-menu-item :crud="$crud" />
+@component('easypanel::crud-menu-item', ['crud' => $crud])
+@endcomponent
 @endforeach
